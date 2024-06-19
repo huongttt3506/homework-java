f33061ca44ebdd2d04be4bd63dd8bb18e060d09e
트란티티엔흐엉3506 <huongttt.ueh@gmail.com>
2024-06-19 16:41:23 +0900

    Add files via upload

---
+++ hello-java/src/HelloJava.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello Huong!!!");
+        System.out.println("Wellcome to Java!");
+        System.out.println("13+7");System.out.println(13 + 7);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextLine());
+
+
+    }
+}
