635d97e540b4b33f3bf120d5ae8773d4e317c11f
huongttt3506 <huongttt.ueh@gmail.com>
2024-06-20 09:36:39 +0900

    Adding Variable Test Class

---
+++ hello-java/src/D1VarAndTypes.java
@@ -0,0 +1,12 @@
+public class D1VarAndTypes {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        System.out.println(a+b);
+        System.out.println(a);
+        System.out.println(b);
+
+        String line = "Hello Variables!";
+        System.out.println(line);
+    }
+}
