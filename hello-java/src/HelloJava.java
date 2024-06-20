8eb6888d34d4297e04f7c5e05f9e50ce1b778ffd
huongttt3506 <huongttt.ueh@gmail.com>
2024-06-20 09:19:40 +0900

    scanner code adding

--- hello-java/src/HelloJava.java
+++ hello-java/src/HelloJava.java
@@ -5,9 +5,11 @@ public class HelloJava {
         System.out.println("Hello Huong!!!");
         System.out.println("Wellcome to Java!");
         System.out.println("13+7");System.out.println(13 + 7);
+//        입력 받고 출력
         Scanner scanner = new Scanner(System.in);
         System.out.println(scanner.nextLine());
+        scanner.nextLine();
 
-
+//    해당 내용 각 주석으로 정리하기
     }
 }
